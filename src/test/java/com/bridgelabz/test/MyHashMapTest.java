ea0f2ca55fd3b2409c5080fe863bcd46cd5bc75b
siva209 <sivanarayana209@gmail.com>
2021-06-10 07:59:38 +0530

    Added:To find frequency of large words in sentence

--- src/test/java/com/bridgelabz/test/MyHashMapTest.java
+++ src/test/java/com/bridgelabz/test/MyHashMapTest.java
@@ -4,9 +4,10 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import com.hashtable.MyHashMap;
+import com.hashtable.MyLinkedHashMap;
 
 public class MyHashMapTest {
-    @Test
+	@Test
     public void givenASentence_WhenWordsAreAddedToList_ShouldReturnWordFrequency(){
         String sentence="To be or not to be";
         MyHashMap<String,Integer> myHashMap=new MyHashMap<>();
@@ -22,6 +23,23 @@ public class MyHashMapTest {
         System.out.println(myHashMap);
         Assert.assertEquals(2,frequency);
     }
+    @Test
+    public void givenSentenceWhenWordsAreAddedToListShouldReturnParanoidFrequency()
+    {
+        String sentence="Paranoids are not paranoid because they are paranoid but"+"because they keep putting themselves deliberately into"+"paranoid avoidable situations";
+        MyLinkedHashMap<String,Integer> myLinkedHashMap=new MyLinkedHashMap<>();
+        String[] words=sentence.toLowerCase().split(" ");
+        for (String word : words )
+        {
+            Integer value =myLinkedHashMap.get(word);
+            if (value==null) value=1;
+            else value=value+1;
+            myLinkedHashMap.add(word,value);
+        }
+        System.out.println(myLinkedHashMap);
+        int frequency=myLinkedHashMap.get("paranoid");
+        System.out.println(frequency);
+        Assert.assertEquals(2,frequency);
+    }
 }
-
 
